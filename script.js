5532e6a44cbec17817b406a8d67a933694eb9000
N-E-O-N-I-X <timuraridi@gmail.com>
2024-11-19 14:45:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,18 +2,18 @@
 function copyText() {
   const button = document.querySelector('.copy-button');
   const buttonText = document.getElementById('button-text');
-  const textToCopy = "Sosal?";
+  const textToCopy = "BFxf3u8VkvuTeR3XqyhmEXEro1N26bLzVbLk99V3pump";
   
   navigator.clipboard.writeText(textToCopy).then(() => {
       console.log('Text copied to clipboard!');
       
 
       button.classList.add('clicked');
-      buttonText.textContent = 'Copied: CA is not generated yet...';
+      buttonText.textContent = 'Copied: BFxf3u8VkvuTeR3XqyhmEXEro1N26bLzVbLk99V3pump';
       
       setTimeout(() => {
           button.classList.remove('clicked');
-          buttonText.textContent = 'Token adress: CA is not generated yet...';
+          buttonText.textContent = 'Token adress: BFxf3u8VkvuTeR3XqyhmEXEro1N26bLzVbLk99V3pump';
       }, 3000);
   }).catch(err => {
       console.error('Failed to copy text: ', err);
